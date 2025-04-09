2d6f548de32117c9e71fe495e000f172fe4f32ac
jeff jia <jarasta.doee@gmail.com>
2025-04-09 19:12:49 +1200

    added lookup class

---
+++ ApplianceLookup.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+
+public class ApplianceLookup {
+    public static void main(String[] args){
+        //file path for the appliances
+        String filePath = "appliances.csv"; 
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            
+
+            
+            
+
+            
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        } 
+    }
+}
